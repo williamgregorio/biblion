124d9210410c56b3ec92d4ddb70aa384f6a00a7a
William <mrwilliamgregorio@yahoo.com>
2024-10-10 10:59:34 -0400

    err support

--- main.go
+++ main.go
@@ -15,9 +15,12 @@ func (p *Page) save() error {
   return os.WriteFile(filename, p.Body, 0600)
 }
 
-func loadPage(title string) *Page {
+func loadPage(title string) (*Page, error) {
   filename := title + ".txt"
   body, _ := os.ReadFile(filename)
+  if err != nil {
+    return nil, err
+  }
   return &Page{Title: title, Body: body}
 }
 
