5ca10149ef55089d1007f96f3b66ad44cc68d4c6
William <mrwilliamgregorio@yahoo.com>
2024-10-10 13:49:54 -0400

    adding save method for page struct

--- main.go
+++ main.go
@@ -3,12 +3,25 @@ package main
 import (
   "fmt"
   "log"
+  "os"
   "net/http"
 )
 
+type Page struct {
+  Title string
+  Body []byte
+}
+
+func (p *Page) save() err {
+  filename := p.title + ".txt"
+  return os.Writefile(filename, p.Body, 0600)
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "Biblion", r.URL.Path[1:])
+  fmt.Fprintf(w, "Biblion %s", r.URL.Path)
 }
 
 func main()  {
+  http.HandleFunc("/", handler)
+  log.Fatal(http.ListenAndServe(":8000", nil))
 }
