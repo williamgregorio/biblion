c2d58b300b82298bf189b31aed4d7e7ebfbb8e4b
William <mrwilliamgregorio@yahoo.com>
2024-10-10 13:13:37 -0400

    handler function

--- main.go
+++ main.go
@@ -2,31 +2,13 @@ package main
 
 import (
   "fmt"
-  "os"
+  "log"
+  "net/http"
 )
 
-type Page struct {
-  Title string
-  Body []byte
-}
-
-func (p *Page) save() error {
-  filename := p.Title + ".txt"
-  return os.WriteFile(filename, p.Body, 0600)
-}
-
-func loadPage(title string) (*Page, error) {
-  filename := title + ".txt"
-  body, err := os.ReadFile(filename)
-  if err != nil {
-    return nil, err
-  }
-  return &Page{Title: title, Body: body}, nil
+func handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Biblion", r.URL.Path[1:])
 }
 
 func main()  {
-  p1 := &Page{Title: "Index", Body: []byte("Biblion")}
-  p1.save()
-  p2, _ := loadPage("Index")
-  fmt.Println(string(p2.Body))
 }
