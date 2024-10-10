ec2148260de3ada947e0fa984a3e7ddbcb012894
William <mrwilliamgregorio@yahoo.com>
2024-10-10 10:57:15 -0400

    load page fn added

--- main.go
+++ main.go
@@ -15,5 +15,11 @@ func (p *Page) save() error {
   return os.WriteFile(filename, p.Body, 0600)
 }
 
+func loadPage(title string) *Page {
+  filename := title + ".txt"
+  body, _ := os.ReadFile(filename)
+  return &Page{Title: title, Body: body}
+}
+
 func main()  {
 }
