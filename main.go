a585e767b3f0e7719ceff21a75e628d26f022cfd
William <mrwilliamgregorio@yahoo.com>
2024-10-10 17:24:51 -0400

    no new var

--- main.go
+++ main.go
@@ -84,7 +84,7 @@ func saveHandler(w http.ResponseWriter, r *http.Request) {
   }
   body := r.FormValue("body")
   p := &Page{Title: title, Body: []byte(body)}
-  err := p.save()
+  err = p.save()
   if err != nil {
     http.Error(w, err.Error(), http.StatusInternalServerError)
     return
