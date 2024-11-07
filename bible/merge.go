63f65d6222b3aef7bf33987e9ad5a554521d3a09
William <mrwilliamgregorio@yahoo.com>
2024-11-07 04:17:06 -0500

    struct declaring

--- bible/merge.go
+++ bible/merge.go
@@ -10,4 +10,25 @@ import (
   "strings"
 )
 
+type Verse struct {
+  Verse string `json:"verse"`
+  Text string `json:"text"`
+}
 
+type Chapter struct {
+  Chapter string `json:"chapter"`
+  Verses []Verse `json:"verses"`
+}
+
+type Book struct {
+  Book string `json:"book"`
+  Chapters []Chapter `json:"chapters"`
+}
+
+type Bible struct {
+  Books []Book `json:"books"`
+}
+
+func main() {
+
+}
