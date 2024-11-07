ea9814ea1737a0a3c7c7f80d6ef317777db85f8c
William <mrwilliamgregorio@yahoo.com>
2024-11-07 03:49:36 -0500

    biblion hed

--- main.go
+++ main.go
@@ -5,5 +5,20 @@ import (
   "net/http"
 )
 
-func main()  {
+type Book struct {
+  Chapter string
+}
+
+func handleRoot(w http.ResponseWriter, req *http.Request) {
+  fmt.Fprintf(w, "Biblion")
+}
+
+func main() {
+  http.HandleFunc("/", handleRoot);
+
+  fmt.Println("Server starting on port :7070")
+  err := http.ListenAndServe(":7070", nil)
+  if err != nil {
+    fmt.Println("Server failed to start:", err)
+  }
 }
